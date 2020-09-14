71add44a03bc3a5bec157e44a12bb386b203f3c2
Jeffrey von Grumbkow <jgrumbkow@gmail.com>
2020-09-14 22:58:21 +0200

    Updates

--- precache-manifest.93b78c8dd441e4ac8a37394761d029f9.js
+++ precache-manifest.93b78c8dd441e4ac8a37394761d029f9.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d52f904d180e0721afcd156d7b694dcb",
+    "revision": "c8f62b5c38c8c69189cb0b4f406cfada",
     "url": "/blink-chat/index.html"
   },
   {
-    "revision": "921212644dff4d1e4b79",
+    "revision": "3bb47f304113dbf4a361",
     "url": "/blink-chat/static/css/2.60c19592.chunk.css"
   },
   {
-    "revision": "d880c22c96dbc67d8fee",
+    "revision": "449a250fa82a81f5d2fa",
     "url": "/blink-chat/static/css/main.2af2ddd3.chunk.css"
   },
   {
-    "revision": "921212644dff4d1e4b79",
-    "url": "/blink-chat/static/js/2.ce4842e2.chunk.js"
+    "revision": "3bb47f304113dbf4a361",
+    "url": "/blink-chat/static/js/2.a79f030e.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/blink-chat/static/js/2.ce4842e2.chunk.js.LICENSE.txt"
+    "url": "/blink-chat/static/js/2.a79f030e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d880c22c96dbc67d8fee",
-    "url": "/blink-chat/static/js/main.c9bda9b5.chunk.js"
+    "revision": "449a250fa82a81f5d2fa",
+    "url": "/blink-chat/static/js/main.bd522e6f.chunk.js"
   },
   {
     "revision": "e45f1e745a07efcb2336",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blink-chat/precache-manifest.a22af6ef1b9adfcc9145799d1a7c6faf.js"
+  "/blink-chat/precache-manifest.93b78c8dd441e4ac8a37394761d029f9.js"
 );
 
 self.addEventListener('message', (event) => {
