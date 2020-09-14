85c477a997f65cc45e143e3c23ed234244f12490
Jeffrey von Grumbkow <jgrumbkow@gmail.com>
2020-09-14 16:07:02 +0200

    Chore: Build

---
+++ build/precache-manifest.bcd40e9508bd0ded572ae044e448b100.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b19e5a2a1fcf5d939e0d0452c00af5a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91c592e21d9bfdbb8028",
+    "url": "/static/css/2.60c19592.chunk.css"
+  },
+  {
+    "revision": "7c316f5bce60f8e5864c",
+    "url": "/static/css/main.b8946652.chunk.css"
+  },
+  {
+    "revision": "91c592e21d9bfdbb8028",
+    "url": "/static/js/2.4a29c2f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4a29c2f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c316f5bce60f8e5864c",
+    "url": "/static/js/main.33f5560a.chunk.js"
+  },
+  {
+    "revision": "4afb6b6dd310786a8ec0",
+    "url": "/static/js/runtime-main.fdf9b097.js"
+  }
+]);
