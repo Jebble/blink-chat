740f97383c6d963b42c7a1fac02df44804a30d2d
Jeffrey von Grumbkow <jgrumbkow@gmail.com>
2020-09-14 22:22:37 +0200

    Updates

---
+++ precache-manifest.f70b6bdcacd9a0c12245cf24a7ff3b01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f2f2123427a63260aaecb9e3852198",
+    "url": "/blink-chat/index.html"
+  },
+  {
+    "revision": "921212644dff4d1e4b79",
+    "url": "/blink-chat/static/css/2.60c19592.chunk.css"
+  },
+  {
+    "revision": "d56233c23fd7835239c1",
+    "url": "/blink-chat/static/css/main.1e3b464c.chunk.css"
+  },
+  {
+    "revision": "921212644dff4d1e4b79",
+    "url": "/blink-chat/static/js/2.ce4842e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blink-chat/static/js/2.ce4842e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d56233c23fd7835239c1",
+    "url": "/blink-chat/static/js/main.09768129.chunk.js"
+  },
+  {
+    "revision": "e45f1e745a07efcb2336",
+    "url": "/blink-chat/static/js/runtime-main.b6ff27af.js"
+  }
+]);
